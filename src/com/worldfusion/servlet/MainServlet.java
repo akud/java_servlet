797b2f7d5138daa530e0296574ee3820d13f7b8e
Alex Kudlick <akudlick@shopittome.com>
2014-04-28 10:49:53 -0700

    MainServlet - intial setup with db connection and mysql connector

--- src/com/worldfusion/servlet/MainServlet.java
+++ src/com/worldfusion/servlet/MainServlet.java
@@ -1,37 +1,42 @@
 package com.worldfusion.servlet;
 
 import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import com.worldfusion.database.DatabaseConnection;
+import com.worldfusion.database.MysqlDatabaseConnection;
 
 /**
  * Servlet implementation class MainServlet
  */
 public class MainServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public MainServlet() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
+    private DatabaseConnection databaseConnection;
+	
+	@Override
+	public void init() throws ServletException {
+		super.init();
+		databaseConnection = new MysqlDatabaseConnection("localhost", "programming_assignment", "root");
+	}
+	
+	@Override
+	public void destroy() {
+		super.destroy();
+		databaseConnection.close();
+	}
 
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+        RequestDispatcher requestDispatcher = request.getRequestDispatcher("main.jsp");
+        requestDispatcher.forward(request, response);
 	}
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-	}
 
 }
