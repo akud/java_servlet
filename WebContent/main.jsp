143e7c5f466a9981a3e205c6d684780cd5764602
Alex Kudlick <akudlick@shopittome.com>
2014-04-28 11:21:25 -0700

    MainServlet - display all cancer references

--- WebContent/main.jsp
+++ WebContent/main.jsp
@@ -1,5 +1,5 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
-    pageEncoding="UTF-8"%>
+    pageEncoding="UTF-8" import="com.worldfusion.models.*,java.util.List"%>
 <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
 <html>
 <head>
@@ -7,6 +7,27 @@
 <title>Worldfusion Programming Exercise</title>
 </head>
 <body>
-	<h1>fud</h1>
+	<table>
+	<thead>
+		<tr>
+			<th>Cancer Type</th>
+			<th>All</th>
+			<th>Year</th>
+			<th>Month</th>
+			<th>Recent</th>
+		</tr>
+	</thead>
+	<tbody>
+		<% for(CancerReference cancerReference : ((List<CancerReference>)request.getAttribute("cancerReferences"))){ %>
+			<tr>
+				<td><%= cancerReference.getType() %></td>
+				<td><%= cancerReference.getAllCount() %></td>
+				<td><%= cancerReference.getYearCount() %></td>
+				<td><%= cancerReference.getMonthCount() %></td>
+				<td><%= cancerReference.getRecentCount() %></td>
+			</tr>
+		<% } %>
+	</tbody>
+	</table>
 </body>
 </html>
--- src/com/worldfusion/servlet/MainServlet.java
+++ src/com/worldfusion/servlet/MainServlet.java
@@ -1,6 +1,7 @@
 package com.worldfusion.servlet;
 
 import java.io.IOException;
+import java.util.List;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -8,8 +9,10 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.worldfusion.dao.CancerReferenceFinder;
 import com.worldfusion.database.DatabaseConnection;
 import com.worldfusion.database.MysqlDatabaseConnection;
+import com.worldfusion.models.CancerReference;
 
 /**
  * Servlet implementation class MainServlet
@@ -34,7 +37,10 @@ public class MainServlet extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        RequestDispatcher requestDispatcher = request.getRequestDispatcher("main.jsp");
+        List<CancerReference> cancerReferences = new CancerReferenceFinder(databaseConnection).getAllCancerReferences();
+	    request.setAttribute("cancerReferences", cancerReferences);
+
+	    RequestDispatcher requestDispatcher = request.getRequestDispatcher("main.jsp");
         requestDispatcher.forward(request, response);
 	}
 
